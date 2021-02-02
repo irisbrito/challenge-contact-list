81c2adbe4a4c4996dc7fea4984842cdda15dec4c
Iris <iris.souza@zup.com.br>
2021-02-02 17:34:57 -0300

    Entrega máxima

--- src/com/br/zup/Main.java
+++ src/com/br/zup/Main.java
@@ -1,5 +1,7 @@
 package com.br.zup;
 
+import org.w3c.dom.ls.LSOutput;
+
 import java.util.*;
 
 public class Main {
@@ -18,20 +20,38 @@ public class Main {
     boolean continuar = true;
 
         while(continuar){
-            System.out.println("Digite um nome para cadastrar");
-            String nome = createScanner().nextLine();
-            System.out.println("Digite o email do contato");
-            String email = createScanner().nextLine();
-            System.out.println("Digite o telefone do contato");
-            String telefone = createScanner().nextLine();
-            listaContatos.put(email, Arrays.asList(nome, email, telefone));
-            System.out.println("Usuário cadastrado! \n Nome: " +nome+" \nEmail: "+ email + "\nTelefone: "+ telefone);
-            System.out.println(listaContatos);
-            System.out.println("Deseja cadastrar mais um contato? Digite sim ou sair para finalizar");
-            String resposta = createScanner().nextLine();
-            if(resposta.equalsIgnoreCase("sair")){
-                continuar = false;
+            System.out.println("Digite 1 para cadastrar contato ou 2 para deletar contato");
+            int respostaMenu = createScanner().nextInt();
+            if(respostaMenu == 1){
+                System.out.println("Digite um nome para cadastrar");
+                String nome = createScanner().nextLine();
+                System.out.println("Digite o email do contato");
+                String email = createScanner().nextLine();
+                if(listaContatos.containsKey(email)){
+                    System.out.println("E-mail já cadastrado!");
+                }
+                System.out.println("Digite o telefone do contato");
+                String telefone = createScanner().nextLine();
+                listaContatos.put(email, Arrays.asList(nome, email, telefone));
+                System.out.println("Usuário cadastrado! \n Nome: " +nome+" \nEmail: "+ email + "\nTelefone: "+ telefone);
+                System.out.println(listaContatos);
+                System.out.println("Deseja cadastrar mais um contato? Digite sim ou sair para finalizar");
+                String resposta = createScanner().nextLine();
+                if(resposta.equalsIgnoreCase("sair")){
+                    continuar = false;
+                }
+            } else if(respostaMenu == 2){
+                System.out.println("Digite o e-mail do contato à ser deletado");
+                String email = createScanner().nextLine();
+                if(listaContatos.containsKey(email)){
+                    listaContatos.remove(email);
+                    System.out.println("Contato deletado com sucesso!");
+                    continuar = false;
+                } else {
+                    System.out.println("O e-mail indicado não está cadastrado");
+                }
             }
+
         }
 
 
